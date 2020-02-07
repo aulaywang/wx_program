9624ba0fc3af856fd853008fecc8805d3ea6b005
aulaywang <839418239@qq.com>
2020-02-07 13:23:04 +0800

    网页请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,59 @@
 // pages/home/home.js
+import request from '../../service/network.js'
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    //1、原生方式发送网络请求
+    this.get_data_origin()
+    //2、封装request发送网络请求
+    //Promise最大的好处是防止出现回调地狱
+    request({
+      url: 'http://httpbin.org/post',
+    }).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  get_data_origin(){
+    //发送网络请求
+    //发送最简单的get请求
+    // wx.request({
+    //   url: 'https://api.bilibili.com/x/relation/stat?vmid=163637592',
+    //   success: function (res) {
+    //     console.log(res)
+    //   }
+    // })
+    //发送带参数的get请求
+    // wx.request({
+    //   url: 'https://api.bilibili.com/x/relation/stat?vmid=163637592',
+    //   data:{
+    //     type: 'sell',
+    //     page: 1
+    //   },
+    //   success: function(res){
+    //     console.log(res)
+    //   }
+    // })
+    //发送post请求
+    // wx.request({
+    //   url: 'http://httpbin.org/post',
+    //   method: 'post',
+    //   data: {
+    //     type: 'sell',
+    //     page: 1
+    //   },
+    //   success: function (res) {
+    //     console.log(res)
+    //   },
+    //   fail: function(err){
+    //     console.log(err)
+    //   },
+    //   complete: function (comp) {
+    //     console.log(comp)
+    //   },
+    // })
   }
 })
